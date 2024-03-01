770e60df50102acc3f58bed04f500d6549e6a441
sylvaincodes <sylvaincodeur@gmail.com>
2024-03-01 23:10:50 +0100

    installed fonts

---
+++ app/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local'
+
+export const courgetteFont = localFont({
+    src:"../public/fonts/Courgette-Regular.ttf"
+})
+
+ 
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+         <h1 className="font-extrabold">
+
+         test our fonts 
+         </h1>
     </main>
   );
 }
