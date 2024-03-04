6d0fcdb3b311acec5184b18989dd952290156f07
sylvaincodes <sylvaincodeur@gmail.com>
2024-03-04 03:06:55 +0100

    add footer component

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,6 @@
 import CtaOne from "@/components/wrappers/cta/cta-one";
 import { CtaThree } from "@/components/wrappers/cta/cta-three";
-import CtaTwo from "@/components/wrappers/cta/cta-two";
-import { NewsletterOne } from "@/components/wrappers/newsletter/newsletter-one";
+import { FooterOne } from "@/components/wrappers/footer/footer-one";
 import TabProductOne from "@/components/wrappers/tabs-products/tabproduct-one";
 import { mergeOpenGraph } from "@/lib/mergeOpenGraph";
 import type { Metadata } from "next";
@@ -10,8 +9,8 @@ export default function Home() {
   return (
     <main className="min-h-screen">
          <TabProductOne/>
-         <CtaOne/>
          <CtaThree/>
+         <CtaOne/>
     </main>
   );
 }
--- components/wrappers/footer/footer-one.tsx
+++ components/wrappers/footer/footer-one.tsx
@@ -1,7 +1,124 @@
+import { Button } from "@/components/ui/button";
+import { Container } from "@/components/ui/container";
+import { Input } from "@/components/ui/forms/input";
+import { Logo } from "@/components/ui/logo";
+import Link from "next/link";
 
-export  const FooterOne = () => {
+export const FooterOne = () => {
   return (
-    <div>FooterOne</div>
-  )
-}
+    <footer className="mt-20 bg-gray-200 py-10">
+      <Container>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-12">
+          <div className="flex flex-col gap-4">
+            <Logo />
+            <p>All right reserved 2024</p>
+          </div>
 
+          <div>
+            <h1 className="uppercase text-2xl text-gray-500">about us</h1>
+            <ul className="mt-4 flex flex-col gap-y-4">
+              <li>
+                <Link className="capitalize" href="#">
+                  about us
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="capitalize" href="#">
+                  store location{" "}
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="capitalize" href="#">
+                  {" "}
+                  contact{" "}
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="capitalize" href="#">
+                  {" "}
+                  order tracking{" "}
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h1 className="uppercase text-2xl text-gray-500">useful links</h1>
+            <ul className="mt-4 flex flex-col gap-y-4">
+              <li>
+                <Link className="capitalize" href="#">
+                  returns
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="capitalize" href="#">
+                  support policy{" "}
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="capitalize" href="#">
+                  {" "}
+                  size guide{" "}
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="capitalize" href="#">
+                  {" "}
+                  Faqs{" "}
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h1 className="uppercase text-2xl text-gray-500">follow us</h1>
+            <ul className="mt-4 flex flex-col gap-y-4">
+              <li>
+                <Link className="capitalize" href="#">
+                  github
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="capitalize" href="#">
+                  youtube{" "}
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="capitalize" href="#">
+                  {" "}
+                  linkedin{" "}
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="capitalize" href="#">
+                  {" "}
+                  twitter{" "}
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-span-2 flex flex-col gap-8">
+            <h1 className="uppercase text-2xl text-gray-500">subscribe</h1>
+            <p>Get E-mail updates about our latest shop and special offers.</p>
+            <div className="flex flex-col gap-8">
+              <Input name="email" type="text" />
+              <Button variant="primary" className="uppercase py-4 w-fit">
+                subscribe
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </footer>
+  );
+};
