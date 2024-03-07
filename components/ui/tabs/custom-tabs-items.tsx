5d700d533be0593fe9ca82924550b15f09fd7dfd
sylvaincodes <sylvaincodeur@gmail.com>
2024-03-07 14:36:54 +0100

    product tabs updated

--- components/ui/tabs/custom-tabs-items.tsx
+++ components/ui/tabs/custom-tabs-items.tsx
@@ -21,7 +21,6 @@ export default function CustomTabsItems({
     <li className={cn('me-2', courgetteFont.className) }>
           <button
             onClick={() => handleclick(title)}
-            href="#"
             className={`capitalize inline-block text-2xl p-4 
                    rounded-t-lg 
                   dark:hover:text-gray-600 font-bold  ${
